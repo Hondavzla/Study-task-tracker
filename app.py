f7632540b820c14ea9f6360152bf71e79ae1c1ee
Ricardo Salcedo <ricardosalcedo@Ricardos-MacBook-Air.local>
2026-03-11 17:31:18 -0400

    Setup flask app and gitignore

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, Mac!</h1><p>My Flask app is alive!</p>"
+if __name__ == "__main__":
+    app.run(debug=True)
